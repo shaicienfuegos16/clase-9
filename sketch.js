5dc28d4a34a6ad7255c5153be9af9e6e699e8679
shaicienfuegos16 <93685045+shaicienfuegos16@users.noreply.github.com>
2021-11-03 16:34:25 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
-
+var cuadrado;
 function setup() {
   createCanvas(400,400);
+  cuadrado = createSprite(200,200,30,30);
 }
 
 function draw() 
 {
   background("red");
 
+if (keyIsDown(LEFT_ARROW)) {
+cuadrado.position.x=cuadrado.position.x-5;
+
+}
+if (keyIsDown(RIGHT_ARROW)) {
+  cuadrado.position.x=cuadrado.position.x+5;
+  
+  }
+  drawSprites();
 }
 
 
